0f38860c3c3cdf7e55525be6a124c846281a328a
manjit-code <manjit.majhi@esyasoft.com>
2025-12-26 12:03:28 +0530

    Basic Manual Token Generation Works

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -24,6 +24,7 @@ namespace SSO_IdentityProvider.API.Controllers
 
 
         [HttpGet("ldap-health")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult LdapHealth()
         {
             try
@@ -54,6 +55,9 @@ namespace SSO_IdentityProvider.API.Controllers
 
 
         [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
             if (request == null || string.IsNullOrEmpty(request.Username) || string.IsNullOrEmpty(request.Password))
